c2cb7c2d4b7631ca4ed056ec2841b2f088c94c53
Renato Bellotti <renato-jun.bellotti@bluewin.ch>
2017-08-23 10:24:21 +0200

    Got simple basic head line working using Font Awesome.

--- freerun_desktop/freerun_desktop/settings.py
+++ freerun_desktop/freerun_desktop/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+# keys for external APIs
+from .api_keys import FONT_AWESOME_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'freerun',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
