2c861bea42f1f8566c720557a47c60e9d72e5118
Renato Bellotti <renato-jun.bellotti@bluewin.ch>
2017-08-30 13:33:22 +0200

    Selection of files is now working.

--- freerun_desktop/freerun/views.py
+++ freerun_desktop/freerun/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
 import socket
+import os
+import json
+from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 def index(request):
-    context = {'ip': socket.gethostbyname(socket.gethostname())}
+    list_of_files = [file for file in os.listdir(settings.DATA_FILE_DIR) if file.endswith('.gpx')]
+    print(json.dumps(list_of_files))
+    context = {
+        'ip': socket.gethostbyname(socket.gethostname()),
+        'list_of_datafiles': json.dumps(list_of_files),
+    }
     return render(request, 'freerun/index.html', context)
